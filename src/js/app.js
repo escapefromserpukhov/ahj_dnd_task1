6c7261352f9ed4b95c99a770ea5b14e1caf8d3ac
Viktoria_finch <85229737+escapefromserpukhov@users.noreply.github.com>
2023-12-15 16:13:26 +0300

    Add files via upload

---
+++ src/js/app.js
@@ -0,0 +1,7 @@
+import { DomControl } from './components/DomControl'
+
+const control = new DomControl()
+control.init()
+// if (!document.querySelector('.item')) {
+//   control.testData()
+// };
---
+++ src/js/components/storage/StorageControl.js
@@ -0,0 +1,31 @@
+import Task from './Task'
+import { createTask } from '../crateTask/createTask.mjs'
+
+export class StorageControl {
+  constructor () {
+    this.storage = []
+    this.blocks = [document.querySelector('.block1'), document.querySelector('.block2'), document.querySelector('.block3')]
+  }
+
+  load () {
+    this.storage = []
+    const data = JSON.parse(localStorage.getItem('Tasks'))
+    if (data) {
+      data.forEach((task) => {
+        this.storage.push(new Task(task.domElement, task.text))
+        createTask(document.querySelector(`.${task.domElement}`), task.text)
+      })
+    }
+  }
+
+  save () {
+    this.storage = []
+    this.blocks.forEach((block) => {
+      block.querySelectorAll('.item').forEach((item) => {
+        this.storage.push(new Task(block.classList[1], item.querySelector('span').innerText))
+      })
+    })
+
+    localStorage.setItem('Tasks', JSON.stringify(this.storage))
+  }
+}
